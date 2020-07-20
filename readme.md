0f80113e916277dd957d556e20aa46200e34a238
Ethan Cowan <ethan.cowan@gmail.com>
2020-07-20 12:43:20 -0600

    Update readme with notes from teddi's live lesson

--- readme.md
+++ readme.md
@@ -12,20 +12,25 @@
 ## While you are beginning to understand Express focus on understanding the following:
 
 * What the difference is between how this was written in Node and how it is written in Express
-* What is in the package.json and how to use scripts and dependencies 
-* How to spin up a basic server 
-* What middleware is, where it goes 
+* What is in the package.json and how to use scripts and dependencies
+* How to spin up a basic server
+  * see teddi's youtube video
+* What middleware is, where it goes
 * What a route handler is and how to write one
-* What params are and how to send them from the client and how to read them on the server (client and server side) 
+  * /quote - the route handler is basically the endpoint
+* What params are and how to send them from the client and how to read them on the server (client and server side)
+  * params are indicated by a colon at the end of an endpoint - the value that is put into the request gets added to the params object
+  * query params are indicated by a ? after a param - get added to a separate query params object, just called query
+  * params is another thing that lives on the request object - you can see it by console logging it
 * Where to find the data sent in a client request
 * How to respond to a request and end the connection
-* How to parse json or urlencoded data sent from the client 
-* What GET/POST/PATCH/DELETE are 
+* How to parse json or urlencoded data sent from the client
+* What GET/POST/PATCH/DELETE are
 * Promises (.then, .catch) and how to use them and return them from a function
 * Callbacks
-* Model/View/Controller and how to pass data between them 
+* Model/View/Controller and how to pass data between them
 * static routes
 * What an endpoint is
-* How to traverse the docs for Node, Express, npm packages 
+* How to traverse the docs for Node, Express, npm packages
 * How to use postman to test your routes
 
--- server.js
+++ server.js
@@ -19,19 +19,29 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+// IMPORTANT - you have to put every use call in the correct order on the server-side
+// here, the flow is like:
+// 1. bodyparser functions
+// 2. route handling
+
+
 //our server is delivering our client side content,
+// client is being served up by the server - this gets started when the server gets created on line 2
+// no cors errors because the client and the server are at the same origin
 // no need for live-server
 app.use(express.static('client'));
 
 //allow CORS requests
-//because of the express.static usage, 
+//because of the express.static usage,
 //we no longer need this
 // app.use(cors());
 
 // parse application/x-www-form-urlencoded
+// if the data that comes in --> if you look at req.body, and you see nothing, then ask yourself if you have body parser... if you do have bodyParser, but you're still not seeing the body, then go to npm and look at the bodyParser package
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
+// another middleware thing to properly parse data that's gonna get added to the request object in the body property
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
@@ -39,9 +49,9 @@ app.get('/', (req, res, next) => {
 })
 
 app.get('/quote', (req, res, next) => {
-  res.send(quotes[getRandomInt(0, quotes.length)]);
+  res.send(quotes[getRandomInt(0, quotes.length -1)]);
 })
-
+// app.use(checkAdminStatus) // a non-existent function that would interrupt everything down the chain if the user doesn't have admin status... could also put this checkAdminStatus in a particular request for a specific endpoint, in which case it would only get invoked as a check if the request resolves inside of that conditional
 app.post('/quote', (req, res, next) => {
   //incoming request should include a quote key
   quotes.push(req.body.quote) // this step wasn't in the node mini sprint
@@ -55,5 +65,5 @@ app.use((req, res, next) => {
 
 app.listen(port,() => {
   console.log(`listening on port ${port}`);
-  
+
 })
